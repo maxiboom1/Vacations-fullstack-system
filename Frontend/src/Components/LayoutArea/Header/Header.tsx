4923e990a20bbef46d3ad141a98c4e01d0cfee8d
Alex Samih-zade <galutziogen@gmail.com>
2023-04-08 14:58:15 +0300

    front init

--- Frontend/src/Components/LayoutArea/Header/Header.tsx
+++ Frontend/src/Components/LayoutArea/Header/Header.tsx
@@ -3,7 +3,7 @@ import "./Header.css";
 function Header(): JSX.Element {
     return (
         <div className="Header">
-			<h1>Full Stack Template...</h1>
+			<h1>Your next vacation starts here!</h1>
         </div>
     );
 }
