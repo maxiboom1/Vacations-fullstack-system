78c34753c1e3bcd1ef2fae44122e38fb3095155d
Alex Samih-zade <galutziogen@gmail.com>
2023-04-08 14:56:10 +0300

    front init

--- Frontend/src/Utils/AppConfig.ts
+++ Frontend/src/Utils/AppConfig.ts
@@ -1,5 +1,9 @@
 class AppConfig {
-    public serverUrl = "http://localhost:4000/api/____/";
+    
+    public registerURL = "http://localhost:4000/api/register";
+    public loginURL = "http://localhost:4000/api/login";
+    public vacationsURL = "http://localhost:4000/api/vacations/";
+
 }
 
 const appConfig = new AppConfig();
