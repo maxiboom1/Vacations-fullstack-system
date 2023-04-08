7445e1e87b053669678eabac4c1db7c04ae11b09
Alex Samih-zade <galutziogen@gmail.com>
2023-04-08 22:52:51 +0300

    AuthState & AuthService

---
+++ Frontend/src/Services/AuthService.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+import UserModel from "../Models/UserModel";
+import appConfig from "../Utils/AppConfig";
+import { AuthActionType, authStore } from "../Redux/AuthState";
+import CredentialsModel from "../Models/CredentialsModel";
+
+
+class AuthService{
+
+    // Register - send user to register URL, get token and store it in authState
+    public async register(user: UserModel): Promise<void>{
+        
+        const response = axios.post<string>(appConfig.registerURL, user);
+
+        const token = (await response).data;
+
+        authStore.dispatch({type: AuthActionType.Register, payload: token});
+
+    }
+
+    // Login - send credentials to login URL, get token and store it in authState
+    public async login(credentials: CredentialsModel): Promise<void> {
+
+        const response = await axios.post<string>(appConfig.loginURL, credentials);
+
+        const token = response.data;
+
+        authStore.dispatch({ type: AuthActionType.Login, payload: token });
+    }
+
+    // Logout - delete token from authState
+    public logout(): void {
+        
+        authStore.dispatch({ type:AuthActionType.Logout });
+
+    }
+
+}
+
+export default AuthService;
