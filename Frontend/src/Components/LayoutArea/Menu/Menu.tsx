149dc1232f426c8af6bd0977c9337e35bb95e5a5
maxiboom1 <galutziogen@gmail.com>
2023-04-20 17:41:57 +0300

    menu events enum and logic

--- Frontend/src/Components/LayoutArea/Menu/Menu.tsx
+++ Frontend/src/Components/LayoutArea/Menu/Menu.tsx
@@ -11,42 +11,39 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import logo from "../../../Assets/Logo/logo-dark.png";
+import { useNavigate } from 'react-router-dom';
+import { AuthActionType, authStore } from '../../../Redux/AuthState';
 
 interface Props {
   window?: () => Window;
 }
 
 function Menu(props: Props):JSX.Element {
+  
+  enum MenuItems {LOGIN = "Login", LOGOUT = "Logout", ABOUT = "About"};
+  const navigate = useNavigate();
+  
+  // Drawer config --------------------------------------------------------------------
   const drawerWidth = 340;
-  const navItems = ["Login", "Logout", "About"];
-  const { window } = props;
+  const navItems: MenuItems[] = [MenuItems.LOGIN, MenuItems.LOGOUT, MenuItems.ABOUT];  const { window } = props; // We secure the values with enum, so nobody will run here not valid values..
   const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  // Function exp => Toggles open/close drawer
   const handleDrawerToggle = () => {setMobileOpen((prevState) => !prevState);}; 
+  const container = window !== undefined ? () => window().document.body : undefined;
 
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: "left" }}>
-      <Typography variant="h6" sx={{ my: 2 }}>MUI</Typography>
-      <Divider />
-      
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding> 
-            <ListItemButton sx={{ textAlign: "center" }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+  // Menu events ---------------------------------------------------------------------- 
+  const menuEvents = {
+    login: () => navigate("/login"),
+    logout: ()=> {
+      authStore.dispatch({type:AuthActionType.Logout});
+      navigate("/greetings");
+    },
+    about: ()=> alert('About in dev now... try later'),
 
-    </Box>
-  );
-
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
+  }
+  
+  const handleMenuClick = (event:string) => {menuEvents[event.toLowerCase() as keyof typeof menuEvents]()}
 
   return (
     <Box sx={{ display: "flex" }}>
@@ -59,13 +56,14 @@ function Menu(props: Props):JSX.Element {
             <MenuIcon />{" "}
           </IconButton>
 
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1, display: { xs: "none", sm: "flex" } } } >
-            MUI
-          </Typography>
+          {/* Site Logo */}
+          <Box sx={{ flexGrow: 1, display: { xs: "none", sm: "flex" } }}>
+            <img width={270} src={logo} alt="Logo" />
+          </Box>
 
           <Box sx={{ display: { xs: "none", sm: "block" } }}>
             {navItems.map((item) => (
-              <Button key={item} sx={{ color: "#fff" }}>
+              <Button key={item} sx={{ color: "#fff" }} onClick={()=> handleMenuClick(item)}>
                 {item}
               </Button>
             ))}
@@ -73,68 +71,25 @@ function Menu(props: Props):JSX.Element {
         </Toolbar>
       </AppBar>
       <Box component="nav">
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{ keepMounted: true }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-        >
-          {drawer}
+        <Drawer container={container} variant="temporary" open={mobileOpen} onClose={handleDrawerToggle} ModalProps={{ keepMounted: true }}
+          sx={{display: { xs: "block", sm: "none" }, "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },}}>
+        
+        {/* Drawer HTML */}
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+          {/* Site Logo */}
+          <Box sx={{ my: 1 }}><img width={270} src={logo} alt="Logo" /></Box>      
+          <Divider />
+          <List>{navItems.map((item) => (
+            <ListItem key={item} disablePadding> 
+              <ListItemButton sx={{ textAlign: "center" }} onClick={()=> handleMenuClick(item)} ><ListItemText primary={item}  /> </ListItemButton> 
+            </ListItem> ))}
+          </List>
+        </Box>
         </Drawer>
       </Box>
-      <Box component="main" sx={{ p: 3 }}>
-        {" "}
-        <Toolbar />{" "}
-      </Box>
+      <Box component="main" sx={{ p: 3 }}>{" "}<Toolbar />{" "}</Box>
     </Box>
   );
 }
 
 export default Menu;
-
-
-
-// import { useNavigate } from "react-router-dom";
-// import "./Menu.css";
-// import { AuthActionType, authStore } from "../../../Redux/AuthState";
-// import Button from '@mui/material/Button';
-// import AppBar from '@mui/material/AppBar';
-// import Box from '@mui/material/Box';
-// import Toolbar from '@mui/material/Toolbar';
-// import logo from "../../../Assets/Logo/logo-dark.png";
-
-// function Menu(): JSX.Element {
-    
-//     const navigate = useNavigate();
-//     function logout(){
-//         authStore.dispatch({type:AuthActionType.Logout});
-//         navigate("/greetings");
-//     }
-//     return (    
-        
-//         <div className="Menu">
-//            <Box>
-//                 <AppBar sx={{ color:"red" }}>
-//                  <Toolbar className="menuBox">   
-                    
-//                         <img className="navbarLogo" src={logo}/>
-//                         <Button variant="text" sx={{ color:"white",fontSize: "16px" }} onClick={logout}>Logout</Button>
-                   
-//                 </Toolbar>
-//                 </AppBar>
-			
-//             </Box>
-//         </div>
-        
-//     );
-// }
-
-// export default Menu;
