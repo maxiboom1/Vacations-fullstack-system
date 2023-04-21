9226e32830fe321b33ef90bb6214c189fe247489
Alex Samih-zade <galutziogen@gmail.com>
2023-04-21 20:34:03 +0300

    new navbar

--- Frontend/src/Components/HomeArea/Greetings/Greetings.tsx
+++ Frontend/src/Components/HomeArea/Greetings/Greetings.tsx
@@ -1,7 +1,7 @@
 import "./Greetings.css";
 import img from "../../../Assets/Images/home-image1.jpg"
 import Button from '@mui/material/Button';
-import { Container } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { authStore } from "../../../Redux/AuthState";
 
@@ -19,14 +19,17 @@ function Greetings(): JSX.Element {
                 <img src={img} alt="" />
                 <Container className= "centered" sx={{fontFamily:"cursive", color:"AppWorkspace", position:"absolute"}}>
                     <h2>Welcome to vacation mania web site! </h2>
-                    <h5>Are you ready to your next vacation?</h5>
+                    <h4>Are you ready to your next vacation?</h4>
                     <div className="authButtons">
                         <Button variant="contained" color="success" sx={{width: 200}} onClick={() => navigate("/login")}>Login</Button>
                         <Button variant="contained" color="primary" sx={{width: 200}} onClick={() => navigate("/register")}>Register</Button> 
                     </div>                   
                 </Container> 
             </div>
-
+            
+            <div>
+                <h1 className="collageGallery">asd</h1>
+            </div>
         </div>
     );
 }
--- Frontend/src/Components/LayoutArea/Menu/Menu.tsx
+++ Frontend/src/Components/LayoutArea/Menu/Menu.tsx
@@ -1,18 +1,3 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-import logo from "../../../Assets/Logo/logo-dark.png";
 import { useNavigate } from 'react-router-dom';
 import { AuthActionType, authStore } from '../../../Redux/AuthState';
 
@@ -28,9 +13,6 @@ function Menu(props: Props):JSX.Element {
   // Drawer config --------------------------------------------------------------------
   const drawerWidth = 340;
   const navItems: MenuItems[] = [MenuItems.LOGIN, MenuItems.LOGOUT, MenuItems.ABOUT];  const { window } = props; // We secure the values with enum, so nobody will run here not valid values..
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-  const handleDrawerToggle = () => {setMobileOpen((prevState) => !prevState);}; 
-  const container = window !== undefined ? () => window().document.body : undefined;
 
   // Menu events ---------------------------------------------------------------------- 
   const menuEvents = {
@@ -46,50 +28,119 @@ function Menu(props: Props):JSX.Element {
   const handleMenuClick = (event:string) => {menuEvents[event.toLowerCase() as keyof typeof menuEvents]()}
 
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar component="nav">
-        <Toolbar>
-          
-          {/* Hamburger Icon on small resolutions */}
-          <IconButton color="inherit" aria-label="open drawer" edge="start" onClick={handleDrawerToggle} sx={{ mr: 2, display: { sm: "none" } }}>
-            <MenuIcon />{" "}
-          </IconButton>
-
-          {/* Site Logo */}
-          <Box sx={{ flexGrow: 1, display: { xs: "none", sm: "flex" } }}>
-            <img width={270} src={logo} alt="Logo" />
-          </Box>
-
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
-            {navItems.map((item) => (
-              <Button key={item} sx={{ color: "#fff" }} onClick={()=> handleMenuClick(item)}>
-                {item}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <Box component="nav">
-        <Drawer container={container} variant="temporary" open={mobileOpen} onClose={handleDrawerToggle} ModalProps={{ keepMounted: true }}
-          sx={{display: { xs: "block", sm: "none" }, "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },}}>
-        
-        {/* Drawer HTML */}
-        <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-          {/* Site Logo */}
-          <Box sx={{ my: 1 }}><img width={270} src={logo} alt="Logo" /></Box>      
-          <Divider />
-          <List>{navItems.map((item) => (
-            <ListItem key={item} disablePadding> 
-              <ListItemButton sx={{ textAlign: "center" }} onClick={()=> handleMenuClick(item)} ><ListItemText primary={item}  /> </ListItemButton> 
-            </ListItem> ))}
-          </List>
-        </Box>
-        </Drawer>
-      </Box>
-      <Box component="main" sx={{ p: 3 }}>{" "}<Toolbar />{" "}</Box>
-    </Box>
+    <div className="Menu">
+      <span>menu</span> 
+    </div>
   );
 }
 
 export default Menu;
+
+
+
+
+
+
+
+
+
+
+
+
+// navbar with mui
+
+// import * as React from 'react';
+// import AppBar from '@mui/material/AppBar';
+// import Box from '@mui/material/Box';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import Divider from '@mui/material/Divider';
+// import Drawer from '@mui/material/Drawer';
+// import IconButton from '@mui/material/IconButton';
+// import List from '@mui/material/List';
+// import ListItem from '@mui/material/ListItem';
+// import ListItemButton from '@mui/material/ListItemButton';
+// import ListItemText from '@mui/material/ListItemText';
+// import MenuIcon from '@mui/icons-material/Menu';
+// import Toolbar from '@mui/material/Toolbar';
+// import Button from '@mui/material/Button';
+// import logo from "../../../Assets/Logo/logo-dark.png";
+// import { useNavigate } from 'react-router-dom';
+// import { AuthActionType, authStore } from '../../../Redux/AuthState';
+
+// interface Props {
+//   window?: () => Window;
+// }
+
+// function Menu(props: Props):JSX.Element {
+  
+//   enum MenuItems {LOGIN = "Login", LOGOUT = "Logout", ABOUT = "About"};
+//   const navigate = useNavigate();
+  
+//   // Drawer config --------------------------------------------------------------------
+//   const drawerWidth = 340;
+//   const navItems: MenuItems[] = [MenuItems.LOGIN, MenuItems.LOGOUT, MenuItems.ABOUT];  const { window } = props; // We secure the values with enum, so nobody will run here not valid values..
+//   const [mobileOpen, setMobileOpen] = React.useState(false);
+//   const handleDrawerToggle = () => {setMobileOpen((prevState) => !prevState);}; 
+//   const container = window !== undefined ? () => window().document.body : undefined;
+
+//   // Menu events ---------------------------------------------------------------------- 
+//   const menuEvents = {
+//     login: () => navigate("/login"),
+//     logout: ()=> {
+//       authStore.dispatch({type:AuthActionType.Logout});
+//       navigate("/greetings");
+//     },
+//     about: ()=> alert('About in dev now... try later'),
+
+//   }
+  
+//   const handleMenuClick = (event:string) => {menuEvents[event.toLowerCase() as keyof typeof menuEvents]()}
+
+//   return (
+//     <Box>
+//       <CssBaseline />
+//       <AppBar component="nav" >
+//         <Toolbar>
+          
+//           {/* Hamburger Icon on small resolutions */}
+//           <IconButton color="inherit" aria-label="open drawer" edge="start" onClick={handleDrawerToggle} sx={{ mr: 2, display: { sm: "none" } }}>
+//             <MenuIcon />{" "}
+//           </IconButton>
+
+//           {/* Site Logo */}
+//           <Box sx={{ flexGrow: 1, display: { xs: "none", sm: "flex" } }}>
+//             <img width={270} src={logo} alt="Logo" />
+//           </Box>
+
+//           <Box sx={{ display: { xs: "none", sm: "block" } }}>
+//             {navItems.map((item) => (
+//               <Button key={item} sx={{ color: "#fff" }} onClick={()=> handleMenuClick(item)}>
+//                 {item}
+//               </Button>
+//             ))}
+//           </Box>
+//         </Toolbar>
+//       </AppBar>
+//       <Box component="nav">
+//         <Drawer container={container} variant="temporary" open={mobileOpen} onClose={handleDrawerToggle} ModalProps={{ keepMounted: true }}
+//           sx={{display: { xs: "block", sm: "none" }, "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },}}>
+        
+//         {/* Drawer HTML */}
+//         <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+//           {/* Site Logo */}
+//           <Box sx={{ my: 1 }}><img width={270} src={logo} alt="Logo" /></Box>      
+//           <Divider />
+//           <List>{navItems.map((item) => (
+//             <ListItem key={item} disablePadding> 
+//               <ListItemButton sx={{ textAlign: "center" }} onClick={()=> handleMenuClick(item)} ><ListItemText primary={item}  /> </ListItemButton> 
+//             </ListItem> ))}
+//           </List>
+//         </Box>
+//         </Drawer>
+//       </Box>
+//       <Box component="main" sx={{ p: 3 }}>{" "}<Toolbar />{" "}</Box>
+//     </Box>
+//   );
+// }
+
+// export default Menu;
