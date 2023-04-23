1c013a7f94b023cc6bd573194799e8eb404ea39f
maxiboom1 <galutziogen@gmail.com>
2023-04-23 12:28:12 +0300

    pagination done

--- Frontend/src/Components/HomeArea/Home/Home.tsx
+++ Frontend/src/Components/HomeArea/Home/Home.tsx
@@ -11,16 +11,29 @@ import { Checkbox, FormControlLabel, Pagination, Stack } from "@mui/material";
 
 function Home(): JSX.Element {
     
-    const [currentPage, setCurrentPage] = useState<number>(1);
-    const cardsPerPage = 3;
-    
     const navigate = useNavigate();     
     const [vacations, setVacations] = useState<VacationModel[]>([]);
     const [activeFilters, setActiveFilters] = useState([]);
-        
+
+    // Pagination
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [pageCount, setPageCount] = useState<number>(1);
+    const cardsPerPage = 10;
+    
+    const calcPagination = () => {
+        const endIndex = (currentPage * cardsPerPage);
+        const startIndex = endIndex - cardsPerPage ; 
+        return vacations.slice(startIndex, endIndex);
+    };
+    const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => { setCurrentPage(value); };
+    
+    // On page start data fetch
     useEffect(()=>{
         dataService.getAllVacations()
-            .then((v)=>{setVacations(v);})
+            .then((v)=>{
+                setVacations(v);
+                setPageCount(Math.ceil(v.length/cardsPerPage));
+            })
             .catch((e)=>{
                 notifyService.error(e); 
                 navigate("/greetings"); // If no token, drop the bastard.
@@ -28,15 +41,16 @@ function Home(): JSX.Element {
             );
     },[]);
 
+    // On filters change logic (must be in useEffect, since state change is async in React)
     useEffect(()=>{
         
         const currentVacations:VacationModel[] = vacationsStore.getState().vacations;
         const now = new Date();
         
         const filterPredicates = {
-            isFollowing: (v: VacationModel)=> v.isFollowing ===1,
-            actualVacations: ()=> (v: VacationModel)=> new Date(v.startDate) > now,
-            startedVacations: (v: VacationModel)=> (new Date(v.startDate) < now) && (new Date(v.endDate) > now),
+            isFollowing: (v: VacationModel) => v.isFollowing ===1,
+            actualVacations: (v: VacationModel) => new Date(v.startDate) > now,
+            startedVacations: (v: VacationModel) => (new Date(v.startDate) < now) && (new Date(v.endDate) > now),
         };
         
         const combinedFilter = (v:VacationModel) => {  
@@ -44,8 +58,10 @@ function Home(): JSX.Element {
         };
         
         const filtered = currentVacations.filter(combinedFilter); 
-
         setVacations(filtered);
+        setPageCount(Math.ceil(filtered.length/cardsPerPage));
+        setCurrentPage(1);
+
 
     },[activeFilters]);
 
@@ -57,10 +73,7 @@ function Home(): JSX.Element {
             setActiveFilters(prevFilters => prevFilters.filter(f => f !== name));
         }
     }
-    const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
-        setCurrentPage(value);
-        console.log(value);
-      };
+        
     return (
         
  
@@ -75,11 +88,11 @@ function Home(): JSX.Element {
 
         <div className="cardsBox">
             
-            <div >{vacations.map((v) => (<CardUI data={v} key={v.vacationId} />))}</div>
+            <div >{calcPagination().map((v) => (<CardUI data={v} key={v.vacationId} />))}</div>
 
             {vacations.length > cardsPerPage&&<div className="paginationController">
                 <Stack spacing={1}>
-                    <Pagination count={10} variant="outlined" shape="rounded" page={currentPage} onChange={handlePageChange}/>
+                    <Pagination count={pageCount} variant="outlined" shape="rounded" page={currentPage} onChange={handlePageChange}/>
                 </Stack>
             </div>}
 
