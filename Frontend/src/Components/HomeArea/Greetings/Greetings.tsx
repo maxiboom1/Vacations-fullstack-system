a803991d4d678e720caf0d125cd7f71d9f427767
maxiboom1 <galutziogen@gmail.com>
2023-05-16 16:54:30 +0300

    welcome page

--- Frontend/src/Components/HomeArea/Greetings/Greetings.tsx
+++ Frontend/src/Components/HomeArea/Greetings/Greetings.tsx
@@ -1,12 +1,9 @@
 import "./Greetings.css";
 import img from "../../../Assets/Images/home-image1.jpg"
-import {  Container } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { authStore } from "../../../Redux/AuthState";
 import { useEffect } from "react";
 
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
 
 function Greetings(): JSX.Element {
         
@@ -20,74 +17,29 @@ function Greetings(): JSX.Element {
         };
 
     },[]);
-    
-    function srcset(image: string, size: number, rows = 1, cols = 1) {
-        return {
-          src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
-          srcSet: `${image}?w=${size * cols}&h=${
-            size * rows
-          }&fit=crop&auto=format&dpr=2 2x`,
-        };
-      }
 
 
-    
-      
-    const itemData = [
-    {
-        img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-        title: 'Breakfast',
-        rows: 2,
-        cols: 2,
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-        title: 'Burger',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-        title: 'Camera',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-        title: 'Burger',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-        title: 'Camera',
-    },
 
-
-    ];
     return (
         
-        <div className="Greetings">
+        <div className="Greetings container mt-5 ">
             
             <div className="welcome">
-                <img src={img} alt="" />
-                <Container className= "centered" sx={{color:"AppWorkspace", position:"absolute"}}>
-                    
-                    <h2>Welcome to vacation Express </h2>
-                    <h4>Are you ready to your next vacation?</h4>   
- 
-                </Container> 
+                <img src={img} alt=""/>
+                <div className="centered ">  
+                    <h1>Welcome to vacation Express </h1>
+                    <br />
+                    <h3>Are you ready to your next vacation?</h3>   
+                </div> 
             </div>
-          
-            <div className="collage">
-                <ImageList sx={{ width: "100%", heigth:"100%"}} variant="quilted" cols={4} rowHeight={121}>
-                    
-                    {itemData.map((item) => (
-                        <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
-                        <img
-                            {...srcset(item.img, 121, item.rows, item.cols)}
-                            alt={item.title}
-                            loading="lazy"
-                        />
-                        </ImageListItem>
-                    ))}
 
-                </ImageList>
+            <div className="bottomTab">
+                <p>Welcome to Paradise Getaways! 
+                Discover your dream vacation with us. From serene beaches to thrilling adventures,
+                we have it all. Unwind, explore, and indulge in unforgettable experiences. 
+                 Let us turn your travel dreams into reality. Book now and create memories that last a lifetime.</p>
             </div>
+            
         </div>
     );
 }
