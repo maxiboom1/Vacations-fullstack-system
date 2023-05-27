05127dda6cffdc31980327b82a3b432a30b31d31
maxiboom1 <galutziogen@gmail.com>
2023-05-27 22:57:14 +0300

    chart

--- Frontend/src/Components/HomeArea/Graph/Graph.tsx
+++ Frontend/src/Components/HomeArea/Graph/Graph.tsx
@@ -11,38 +11,33 @@ function Graph(): JSX.Element {
     const vacations = vacationsStore.getState().vacations;
     const xValues = [];
     const yValues = [];
-    const barColors = [];
-    for (const v of vacations){
+    for (const v of vacations) {
       xValues.push(v.destination);
       yValues.push(v.followersCount);
-      barColors.push("green");
     }
+
+    const maxCount = Math.max(...yValues); // Find the maximum integer value in yValues
 
     const ctx = chartRef.current?.getContext("2d");
     if (ctx) {
       new Chart(ctx, {
         type: "bar",
-        data: {
-          labels: xValues,
-          datasets: [
-            {
-              backgroundColor: barColors,
-              data: yValues,
-            },
-          ],
+        data: { labels: xValues, datasets: [ { data: yValues} ] },
+        options: { plugins: {
+          title: {display: true, text: "Vacations popularity"}, 
+          legend: {display: true}} 
         },
-        options: {
-          legend: { display: false },
-          title: {
-            display: true,
-            text: "Vacations popularity",
-          },
-        } as any,
       });
     }
   }, []);
 
-  return <div className="graphBox"><canvas ref={chartRef} className="Graph"></canvas></div>;
+  return (
+    <div className="graphBox">
+      <h2>Followers statistics</h2>
+      <br />
+      <canvas ref={chartRef} className="Graph"></canvas>
+    </div>
+  );
 }
 
 export default Graph;
